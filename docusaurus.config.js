183052b0438a19ffc18431ff4ffb6752777310d6
Yangshun Tay <tay.yang.shun@gmail.com>
2023-06-06 13:03:11 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/defog-ai/defog-docs/tree/main/docs/',
+            'https://github.com/defog-ai/defog-docs/blob/main/docs/',
         },
         blog: false,
         theme: {
